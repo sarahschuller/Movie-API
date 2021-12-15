d470babe4343b0d4e320201be74723d2df237419
sarahschuller <sarahmichele91@gmail.com>
2021-12-15 18:07:42 -0500

    added array with top 10 movies

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-const express = require('express');
+const express = require('express'); //load express module
 const app = express();
+
+//top 10 movies array
+let movies=[
+  {
+    title: 'Suspiria',
+    year: '1977'
+  },
+  {
+    title: 'A Nightmare on Elm Street',
+    year: 1984
+  },
+  {
+    title: 'Get Out',
+    year: 2017
+  },
+  {
+    title: 'Hereditary',
+    year: 2018
+  },
+  {
+    title: 'It',
+    year: 2017
+  },
+  {
+    title: 'Midsommar',
+    year: 2019
+  },
+  {
+    title: 'Carrie',
+    year: 1976
+  },
+  {
+    title:'Train to Busan (Busanhaeng)',
+    year: 2016
+  },
+  {
+    title:'Halloween',
+    year: 1978
+  },
+  {
+    title:'Misery',
+    year: 1990
+  },
+];
