d784cf83e9095640fbe09629039b689a31943219
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-01-15 11:30:32 -0500

    allow all domains to access

--- index.js
+++ index.js
@@ -12,19 +12,7 @@ const Users = Models.User;
 
 // require CORS
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ 
-        // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 // require express-validator
 const { check, validationResult } = require('express-validator');
