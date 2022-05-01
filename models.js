808e6f164ce27c47116726603e667db739697028
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-05-01 17:55:11 -0400

    update notes

--- models.js
+++ models.js
@@ -1,3 +1,14 @@
+/**
+ * @file This file will implement schemas for documents in the movies and users collections in
+ * the myFlix database. Models are created through these schemas, which are used in http requests
+ * to API endpoints to read, update, create and delete these documents from the database. Mongoose is
+ * connected to the database using the connect method in the index file.
+ * @requires mongoose Connects the app to the database and implements data schemas using models.
+ * @requires bcrypt This is  used to implement secutiry and encryption on user passwords.
+ */
+
+
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
